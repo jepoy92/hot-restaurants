bf98820fba1fbdc29283876282de056f06f13eac
msaviola <67287151+msaviola@users.noreply.github.com>
2020-09-10 20:35:37 -0700

    working website

--- server.js
+++ server.js
@@ -39,11 +39,11 @@ var reservations = [
   
   // Basic route that sends the user first to the AJAX Page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
   });
   
   app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
+    res.sendFile(path.join(__dirname, "make.html"));
   });
   
   // Displays all characters
